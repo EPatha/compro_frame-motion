2a17ce99bf6abd362d2516f433ce26c18a91542b
Ephesians Prismaranatha <ephesiansprismaranatha@gmail.com>
2025-05-21 23:36:23 +0700

    update About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-'use client';  // Menandakan bahwa ini adalah Client Component
-
-import { motion } from 'framer-motion';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <style jsx>{`
-        /* Hapus watermark NEXT.js */
-        .nextjs-watermark {
-          display: none;
-        }
-      `}</style>
-      <main className="min-h-screen bg-black flex items-center justify-center">
-        <motion.h1
-          className="text-4xl text-white"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          Selamat datang di Mekanikace!
-        </motion.h1>
-      </main>
-    </>
+    <div>
+      <Link href="/About">mmmm</Link>
+    </div>
   );
 }
